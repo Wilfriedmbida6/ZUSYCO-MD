7c4f69ba8314d9f07b55d1382a0b053fbaab691f
Wilfriedmbida6 <145515251+Wilfriedmbida6@users.noreply.github.com>
2024-04-16 05:22:18 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,10 +16,12 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NUMBER: "237678507431",
+  SUDO_NUMBERS : "678507431",
+  OWNER_NAME: "『 🤴𝓦𝓲𝓵𝓯𝓻𝓲𝓮𝓭 𝓶𝓫𝓲𝓭𝓪🤴
+ 』",
+  BOT_NAME : "🤴🅼🅾🅽🆂🆃🅴🆁 🅺🅸🆁🅰🤴
+",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
@@ -32,16 +34,17 @@ module.exports = {
     },
   DESABLE_WELCOME_GOODBYE_JIDS : [],
   TIME_ZONE :  "Asia/Colombo",
-  WORK_TYPE : "public",
+  WORK_TYPE : "private",
   WORK_MODE : "onlygroups",
   WORK_MODE_MESSAGE_SEND : false,
-  CAPTION : '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ᴢᴜꜱʏᴄᴏ ᴍᴅ ʙᴏᴛ',
-  STICKER_PACK : '[ ZUSYCO ]',
+  CAPTION : '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ 🤴🅼🅾🅽🆂🆃🅴🆁 🅺🅸🆁🅰🤴
+',
+  STICKER_PACK : '[🤴🅼🅾🅽🆂🆃🅴🆁🤴 ]',
   BAD_WORDS : 'fuck,pussy',
   ANTI_BAD : false,
   ANTI_LINKS : 'chat.whatsapp.com',
-  ANTI_LINK : false,
-  ANTI_LINK_KICK : false,
+  ANTI_LINK : true,
+  ANTI_LINK_KICK : true,
   MAX_SIZE : 100,
   MOROCCO_NUMBERS : {
         BLOCK : false,
@@ -54,8 +57,8 @@ module.exports = {
   },
   BOT_OFFLINE : false,
   INBOX_BLOCK : false,
-  AUTO_REACT : false,
-  OWNER_REACT : '🤵‍♂️',
+  AUTO_REACT : true,
+  OWNER_REACT : '🤵‍♂️😉😇😎🤓🫡🙄🥴',
   AUTO_BGM : false,
   BGM : {
     "chaio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-caio.mp3",
